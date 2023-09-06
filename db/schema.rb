78f7ca50b1b7b5199bc9e3addbdb4854b29e3168
JenniLJ <48532943+JenniLJ@users.noreply.github.com>
2023-09-06 17:57:25 -0500

    integrating babel - discovering retired webpacker

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_06_211831) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_06_215222) do
+  create_table "conversations", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
@@ -23,6 +28,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_06_211831) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
